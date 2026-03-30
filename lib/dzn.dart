8f73e6fc9b5d44d89fc494907f22093e309fbeff
DINGXINGYI <1604492619@qq.com>
2026-03-30 16:03:21 +0900

    start

---
+++ lib/dzn.dart
@@ -0,0 +1,7 @@
+void main(){
+  String firstname = 'Gildong';
+  String lastname = 'Hong';
+  String fullname="$lastname $firstname";
+  print("name is $fullname");
+}
+
---
+++ lib/num.dart
@@ -0,0 +1,9 @@
+void main(){
+  int num1 = 10;
+  double pi = 3.14;
+  double num3 = num1*pi;
+  num num4 = num1*pi;
+print('object');
+print("double leixing = $num3");
+print("num leixing = $num4");
+}
